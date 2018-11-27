39d8a866623b5725eb82ae5193714996811205a1
ItzTechZack <44208067+ItzTechZack@users.noreply.github.com>
2018-11-27 19:12:46 +0000

    Database Connection Added

--- server/index.js
+++ server/index.js
@@ -5,3 +5,10 @@ app.listen(8080, err => {
   if (err) throw err;
   console.log('Hello');
 });
+
+const mongoose = require("mongoose");
+
+// Database Connection
+mongoose.connect('', {
+    useNewUrlParser: true
+});
